681cd1400031d1f0e2227a559540aaa9120bb531
Eliezer <eli.defilicibus@gmail.com>
2020-07-27 19:11:59 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC App from c# Course.";
+            ViewData["Aluno"] = "Eliezer Defilicibus";
             return View();
         }
 
